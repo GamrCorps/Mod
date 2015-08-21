640d4d165feb920485a8ea7898430ec07a7987fa
Seasicknormal41 <neptunesblade@gmail.com>
2015-08-20 21:09:42 -0700

    Flying carpet got more major changes!

--- src/main/java/src/enigma/calin/items/ItemFlyingCarpet.java
+++ src/main/java/src/enigma/calin/items/ItemFlyingCarpet.java
@@ -1,8 +1,10 @@
 package src.enigma.calin.items;
 import cpw.mods.fml.common.Mod;
+import jdk.nashorn.internal.ir.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
@@ -16,13 +18,13 @@ import net.minecraft.world.World;
 
 /**
  * Created by Calin on 8/17/2015.
- */
+ **/
 public class ItemFlyingCarpet extends Item
 {
     boolean isEnabled;
     boolean toggle;
-    int x;
     int y;
+    int toggleBlock;
 
     public ItemFlyingCarpet()
     {
@@ -36,19 +38,20 @@ public class ItemFlyingCarpet extends Item
     {
         this.isEnabled = false;
         this.toggle = false;
-        int x = 0;
-        y = -3;
+        this.y = -3;
+        this.toggleBlock = 0;
     }
 
     @Override
     public ItemStack onItemRightClick(ItemStack itemstack, World world, EntityPlayer thePlayer)
     {
-        x += 1;
 
-        if(x == 4)
+        if (thePlayer.worldObj.isRemote)
         {
-            x = 0;
+            return itemstack;
         }
+
+        this.toggle = !this.toggle;
 
         return itemstack;
     }
@@ -59,7 +62,7 @@ public class ItemFlyingCarpet extends Item
         super.onUpdate(stack, world, thePlayer, par4, par5);
         {
             EntityPlayer player = (EntityPlayer) thePlayer;
-            y = -3;
+            this.y = -1;
 
 
             //=== Possible implementation ===
@@ -68,31 +71,38 @@ public class ItemFlyingCarpet extends Item
 
             if (Keyboard.isKeyDown(sneak))
             {
-                y = -4;
+
             }
             else
             {
-                y = -3;
+
             }*/
             //===============================
 
-            if(x == 2)
+
+            if (thePlayer.worldObj.isRemote)
             {
+                return;
+            }
+
+            if(this.toggle)
+            {
+                    this.toggleBlock = 0;
+
+                    world.setBlock((int) player.posX, (int) player.posY + y, (int) (player.posZ), Blocks.glass);
+                    world.setBlock((int) Math.round(player.posX) + 1, (int) Math.round(player.posY) + y, (int) Math.round(player.posZ), Blocks.glass);
+                    world.setBlock((int) Math.round(player.posX) - 1, (int) Math.round(player.posY) + y, (int) Math.round(player.posZ), Blocks.glass);
+                    world.setBlock((int) Math.round(player.posX), (int) Math.round(player.posY) + y, (int) Math.round(player.posZ + 1), Blocks.glass);
+                    world.setBlock((int) Math.round(player.posX), (int) Math.round(player.posY) + y, (int) Math.round(player.posZ - 1), Blocks.glass);
+                    world.setBlock((int) Math.round(player.posX + 1), (int) Math.round(player.posY) + y, (int) Math.round(player.posZ + 1), Blocks.glass);
+                    world.setBlock((int) Math.round(player.posX + 1), (int) Math.round(player.posY) + y, (int) Math.round(player.posZ - 1), Blocks.glass);
+                    world.setBlock((int) Math.round(player.posX - 1), (int) Math.round(player.posY) + y, (int) Math.round(player.posZ - 1), Blocks.glass);
+                world.setBlock((int) Math.round(player.posX - 1), (int) Math.round(player.posY) + y, (int) Math.round(player.posZ + 1), Blocks.glass);
+
                 //=== Possible implementation ===
                 //player.addPotionEffect(new PotionEffect(Potion.moveSpeed.id, 15, 3));
                 //player.addPotionEffect(new PotionEffect(Potion.jump.id, 3, 3));
                 //===============================
-
-                world.setBlock((int) Math.round(player.posX), (int) Math.round(player.posY) + y, (int) Math.round(player.posZ), Blocks.glass, 0, 2);
-                world.setBlock((int) Math.round(player.posX) + 1, (int) Math.round(player.posY) + y, (int) Math.round(player.posZ), Blocks.glass, 0, 2);
-                world.setBlock((int) Math.round(player.posX) - 1, (int) Math.round(player.posY) + y, (int) Math.round(player.posZ), Blocks.glass, 0 ,2);
-                world.setBlock((int) Math.round(player.posX), (int) Math.round(player.posY) + y, (int) Math.round(player.posZ + 1), Blocks.glass, 0 ,2);
-                world.setBlock((int) Math.round(player.posX), (int) Math.round(player.posY) + y, (int) Math.round(player.posZ - 1), Blocks.glass, 0 ,2);
-                world.setBlock((int) Math.round(player.posX + 1), (int) Math.round(player.posY) + y, (int) Math.round(player.posZ + 1), Blocks.glass, 0 ,2);
-                world.setBlock((int) Math.round(player.posX + 1), (int) Math.round(player.posY) + y, (int) Math.round(player.posZ - 1), Blocks.glass, 0 ,2);
-                world.setBlock((int) Math.round(player.posX - 1), (int) Math.round(player.posY) + y, (int) Math.round(player.posZ - 1), Blocks.glass, 0 ,2);
-                world.setBlock((int) Math.round(player.posX - 1), (int) Math.round(player.posY) + y, (int) Math.round(player.posZ + 1), Blocks.glass, 0 ,2);
-
             }
         }
     }
